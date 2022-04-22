8ae9a92100ec81d3bcc89afdf561459ecda7281c
Hola <fernando.fmet@gmail.com>
2022-04-22 12:34:51 -0500

    Ultima version, delimitar area con 4 puntos

--- lib/src/vistas/delimitArea.dart
+++ lib/src/vistas/delimitArea.dart
@@ -1,9 +1,6 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../providers/location_provider.dart';
 import '../providers/map_provider.dart';
 
 class DelimitAreaView extends StatefulWidget {
@@ -14,8 +11,9 @@ class DelimitAreaView extends StatefulWidget {
 }
 
 class _DelimitAreaViewState extends State<DelimitAreaView> {
-
-  final _initialCameraPosition = const CameraPosition(target: LatLng(16.23055717297992, -93.9072327009249), zoom: 12);
+  
+  final _initialCameraPosition = const CameraPosition(target: LatLng(16.23055717297992, -93.9072327009249), zoom: 8);
+  final _controller = MapProvider();
   List<Marker> markers = [];
   List<LatLng> latlngs = [];
   int id = 1;
@@ -32,27 +30,38 @@ class _DelimitAreaViewState extends State<DelimitAreaView> {
           color: Colors.transparent,
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.height,
-          child: GoogleMap(
-            initialCameraPosition: _initialCameraPosition,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: true,
-            mapType: MapType.normal,
-            onTap: (LatLng position) {
-              Marker newMarker = Marker(
-                markerId: MarkerId('$id'),
-                position: LatLng(position.latitude, position.longitude),
-                infoWindow: InfoWindow(title: 'Marcador $id en $position'),
-                icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
-              );
-              markers.add(newMarker);
-              latlngs.add(position);
-              id = id + 1;
-              setState(() {
-                print('Posicion del marcador nuevo $position');  
-              });
-              print('Lista de marcadores $latlngs');
-            },
-            markers: markers.map((e) => e).toSet(),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: GoogleMap(
+                  onMapCreated: _controller.onMapCreated,
+                  initialCameraPosition: _initialCameraPosition,
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  mapType: MapType.satellite,
+                  onTap: (LatLng position) {
+                    if (id < 5) {
+                      Marker newMarker = Marker(
+                        markerId: MarkerId('$id'),
+                        position: LatLng(position.latitude, position.longitude),
+                        infoWindow: InfoWindow(title: 'Marcador $id en $position'),
+                        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
+                      );
+                      markers.add(newMarker);
+                      latlngs.add(position);
+                      id = id + 1;
+                      setState(() {
+                        print('Posicion del marcador nuevo $position');  
+                      });
+                      print('Lista de marcadores $latlngs');
+                    }else{
+                      print('Lista de marcadores $latlngs');
+                    }
+                  },
+                  markers: markers.map((e) => e).toSet(),
+                )
+              )
+            ],
           )
         ),
       ),
